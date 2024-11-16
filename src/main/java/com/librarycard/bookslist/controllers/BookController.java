a03fe02257e5e4e5428e92ea7419822ed1a5cbc7
ThayOlive <thl.oliver.1999@gmail.com>
2024-11-16 15:03:09 -0300

    Obtero nome pelo ID

--- src/main/java/com/librarycard/bookslist/controllers/BookController.java
+++ src/main/java/com/librarycard/bookslist/controllers/BookController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.librarycard.bookslist.dto.BookDTO;
 import com.librarycard.bookslist.dto.BookMinDTO;
 import com.librarycard.bookslist.services.BookServices;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -19,6 +21,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BookController {
     @Autowired
     private BookServices bookService;
+
+    @GetMapping (value = "/{id}")
+    public BookDTO findById(@PathVariable Long id) {
+       BookDTO result = bookService.findById(id);
+       return result;
+   }
 
     @GetMapping
      public List<BookMinDTO> findAll() {
--- src/main/java/com/librarycard/bookslist/services/BookServices.java
+++ src/main/java/com/librarycard/bookslist/services/BookServices.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.librarycard.bookslist.dto.BookMinDTO;
 import com.librarycard.bookslist.entities.Book;
 import com.librarycard.bookslist.repositories.BookRepository;
+import com.librarycard.bookslist.dto.BookDTO;
 
 @Service
 public class BookServices {
@@ -15,6 +17,14 @@ public class BookServices {
     @Autowired
     private BookRepository bookRepository;
 
+    @Transactional (readOnly = true)
+    public BookDTO findById(Long id) {
+        Book result = bookRepository.findById(id).get();
+        BookDTO dto = new BookDTO(result);
+        return dto;
+    }
+
+    @Transactional (readOnly = true)
     public List <BookMinDTO> findAll() {
         List<Book> result = bookRepository.findAll();
         List<BookMinDTO> dto = result.stream().map(x -> new BookMinDTO(x)).toList();
